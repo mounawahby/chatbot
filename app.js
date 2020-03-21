3bc18cf74588491b05442f2b8036ee1c34af636b
Mouna WAHBY <mouna.wahby@edu.devinci.fr>
2020-03-21 13:49:03 +0100

    reverse string display

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
+   
 });
 
 app.listen(3000, function(){
