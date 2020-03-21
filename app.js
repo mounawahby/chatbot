de04ce6ddceb4d85132d3b4b5ae1a25a2d82cf2d
DamienBZH <ily.damien@gmail.com>
2020-03-21 14:43:55 +0100

    Add POST Question

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
-   
+});
+
+app.post('Question', function(req, res){
+    console.log(req);
+    res.send("Reponse du serveur");
 });
 
 app.listen(3000, function(){
