e35782b86fce4807abe425b248206a8311a7d4d9
DamienBZH <ily.damien@gmail.com>
2020-03-23 16:41:01 +0100

    fuse.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const chalk = require('chalk');
 const path = require('path');
 const bodyParser = require('body-parser');
+const Fuse = require('fuse.js');
 const actors = require('./src/routes/actors');
 
 var app = express();
@@ -20,10 +21,28 @@ app.get('/', function(req, res){
 app.post('/Question', function(req, res){
     const actorName = req.body.question;
     const actor = actors.find(item => item.Name == actorName);
-    console.info(actor);
-    const movies = actor.Movies;
-   
+    
+    if (actor != null){
+        console.info(actor);
+        const movies = actor.Movies;
         res.send(movies);
+    }else
+    {
+        const options = {
+            keys: ['Name']
+          };
+        const fuse = new Fuse(actors, options);
+        const searchResult = fuse.search(actorName);
+        if(searchResult.length > 0){
+            console.log(searchResult);
+            const firstResult = searchResult[0].item;
+            console.log(firstResult);
+            console.log(firstResult.Name);
+            result = "Avez-vous voulu dire " + firstResult.Name + "?";
+        }
+    }
+    res.send("Je n'ai pas compris votre question");
+    
       
     
    // res.render('index', {liste:['a','b'],title:'ttt'});
